19d19b615ea051f933c191cd124e1c693c73e3d0
AlbireoSJ <alterf@kakao.com>
2023-10-11 00:47:18 +0900

    1st v1

---
+++ Test_00.cpp
@@ -0,0 +1,15 @@
+//
+//  Test_00.cpp
+//  Test
+//
+//  Created by Albireo on 10/11/23.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello!" << endl;
+    return 0;
+}
