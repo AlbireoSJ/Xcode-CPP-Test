bbd68822e23a7a56f22fcd893f6c6794e4342afe
AlbireoSJ <alterf@kakao.com>
2023-10-16 15:32:59 +0900

    3rd v3

--- Test_00.cpp
+++ Test_00.cpp
@@ -10,6 +10,9 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int age;
+    cout << "나이를 입력하세요." << endl;
+    cin >> age;
+    cout << age << "살" << endl;
     return 0;
 }
