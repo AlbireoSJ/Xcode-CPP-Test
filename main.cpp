26b253160fa33d6574ca4a02fdcdb0bd058c61a2
AlbireoSJ <alterf@kakao.com>
2023-10-16 15:33:24 +0900

    1st v1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Albireo on 10/11/23.
+//
+
+/*
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+*/
